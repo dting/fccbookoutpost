9c5254d88c5d9d0b47fccc75cae588e7d5958ccf
Dennis Ting <dennisting@gmail.com>
2015-07-20 21:14:46 -0700

    Added ocean as a theme.

--- client/app/app.js
+++ client/app/app.js
@@ -10,11 +10,38 @@ var app = angular.module('fccbookoutpostApp', [
 ]);
 
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider,
-    $httpProvider) {
+    $httpProvider, $mdThemingProvider) {
   $urlRouterProvider.otherwise('/');
 
   $locationProvider.html5Mode(true);
   $httpProvider.interceptors.push('authInterceptor');
+
+  $mdThemingProvider.definePalette('ocean', {
+    50: 'bf616a',
+    100: 'd08770',
+    200: 'ebcb8b',
+    300: 'a3be8c',
+    400: '96b5b4',
+    500: '8fa1b3',
+    600: 'b48ead',
+    700: 'ab7967',
+    800: '2b303b',
+    900: '343d46',
+    A100: '4f5b66',
+    A200: '65737e',
+    A400: 'a7adba',
+    A700: 'eff1f5',
+    contrastDefaultColor: 'light',
+    contrastDarkColors: [
+      '50', '100',
+      '200', '300', '400', 'A100'
+    ],
+    contrastLightColors: undefined
+  });
+  $mdThemingProvider.theme('default')
+      .primaryPalette('ocean', {
+        default: '900'
+      });
 });
 
 app.factory('authInterceptor',
